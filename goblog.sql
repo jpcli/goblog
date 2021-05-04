fcfa1d2ff5f3495499b060c1f950f66ad1d749ca
jpcli <jpcli888@gmail.com>
2021-05-04 21:28:08 +0800

    [Style]数据库创建命令

--- goblog.sql
+++ goblog.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 本地mysql8.0
- Source Server Type    : MySQL
- Source Server Version : 80012
- Source Host           : localhost:3306
- Source Schema         : goblog_new
-
- Target Server Type    : MySQL
- Target Server Version : 80012
- File Encoding         : 65001
-
- Date: 22/03/2021 13:06:32
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
